c39f68679e8054405b0694ea08bdb4ff7a71813f
Jeremy White <jcwhite414@gmail.com>
2024-06-29 12:30:57 -0500

    adding tailwind to dev

--- main.go
+++ main.go
@@ -8,15 +8,22 @@ import (
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	data := "Hello World"
-	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/card.html"))
+	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/sign-in-form.html"))
 	
 	tmpl.Execute(w, data)
 }
 
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/{$}", helloWorld)
+	mux.HandleFunc("/", helloWorld)
+
+	// Serve up static css files
+	fs := http.FileServer(http.Dir("./style"))
+
+	// Strip prefix, otherwise it is passed to the fs
+	mux.Handle("/style/", http.StripPrefix("/style", fs))
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		panic(err)
